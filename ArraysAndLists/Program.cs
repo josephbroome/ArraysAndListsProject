35dfe08be62c3817132cfff81f4de56b40a72217
Joseph Broome <josephlloydbroome@gmail.com>
2022-06-21 12:48:04 -0700

    homework

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace ArraysAndLists
 {
     class Program
@@ -9,12 +9,41 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
+            var numbers = new int[] { 1, 2, 3, 4, 5, 6, 8, 9, 10 };
 
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
 
+            var evens = new List<int>();
+           
+            var odds= new List<int>();
+
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    evens.Add(num);
+                }
+                else
+                {
+                    odds.Add(num);
+                }
+
+            }
+
+
+            foreach(var item in evens)
+            {
+                Console.WriteLine($"{item} is an even number!");
+            }
+
+            foreach (var item in odds)
+            {
+                Console.WriteLine($"{item} is an odd number!");
+            }
+            
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
              *  if a number is even or odd.
